444617595538ef0635aed226272473c7c902d51a
Klaexis <alecklim1245@gmail.com>
2023-03-13 17:56:51 +0800

    Update SignUpActivity.kt

--- app/src/main/java/com/mobdeve/s11/group11/mco2/SignUpActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group11/mco2/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.mobdeve.s11.group11.mco2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.mobdeve.s11.group11.mco2.databinding.ActivitySignupBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -10,5 +12,19 @@ class SignUpActivity : AppCompatActivity() {
 
         val viewBinding : ActivitySignupBinding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        var tv_login = findViewById<TextView>(R.id.login_link)
+
+        //SignUp Button set data of user and go to the Login Page and Activity
+        viewBinding.signupBtn.setOnClickListener {
+            val signupIntent : Intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+            startActivity(signupIntent)
+        }
+
+        //Login link to go to Login page and activity
+        tv_login.setOnClickListener {
+            val loginIntent : Intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 }
