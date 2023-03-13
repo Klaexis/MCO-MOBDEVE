d2e070c9813573cb2a262bbe4c5a61d020420b61
Abriece <abrience_liao@dlsu.edu.ph>
2023-03-13 19:37:32 +0800

    Updated Calculator and Signup
    Calculator
    + Dropdown List for Activities
    - Input MET
    
    Signup
    - Gender

--- app/src/main/java/com/mobdeve/s11/group11/mco2/CalculatorActivity.kt
+++ app/src/main/java/com/mobdeve/s11/group11/mco2/CalculatorActivity.kt
@@ -2,9 +2,19 @@ package com.mobdeve.s11.group11.mco2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class CalculatorActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val autotextview = findViewById<AutoCompleteTextView>(R.id.actv_activitySelected)
+        val activities = resources.getStringArray(R.array.activities)
+        val adapter = ArrayAdapter(this,R.layout.activity_list, activities)
+        autotextview.setAdapter(adapter)
+
     }
 }
