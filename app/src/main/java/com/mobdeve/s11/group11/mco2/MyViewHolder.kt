c50d1f7411ad3bf38287f6a2c3d0fe7d036c7e67
Klaexis <alecklim1245@gmail.com>
2023-03-14 23:32:19 +0800

    Added changes
    Added activity in progress
    Fixed the data for progress
    MyViewHolder code is placed temporarily

--- app/src/main/java/com/mobdeve/s11/group11/mco2/MyViewHolder.kt
+++ app/src/main/java/com/mobdeve/s11/group11/mco2/MyViewHolder.kt
@@ -5,7 +5,9 @@ import com.mobdeve.s11.group11.mco2.databinding.ItemListBinding
 
 class MyViewHolder(private val viewBinding: ItemListBinding): RecyclerView.ViewHolder(viewBinding.root) {
     fun bindData(progress: Progress){
+        viewBinding.itemActivity.text = progress.activityMET
         viewBinding.itemDistance.text = progress.distanceTraveled
-
+        viewBinding.itemTime.text = progress.timeElapsed
+        viewBinding.itemCalBurn.text = progress.caloriesBurned
     }
 }
--- app/src/main/java/com/mobdeve/s11/group11/mco2/ProgressGenerator.kt
+++ app/src/main/java/com/mobdeve/s11/group11/mco2/ProgressGenerator.kt
@@ -5,12 +5,12 @@ class ProgressGenerator {
         fun loadProgress(): ArrayList<Progress> {
             val data = ArrayList<Progress>()
 
-            data.add(Progress(1000, 60, 250.78f, "aleck@gmail.com"))
-            data.add(Progress(750, 60, 120.56f, "aleck@gmail.com"))
-            data.add(Progress(1300, 70, 270.32f, "aleck@gmail.com"))
-            data.add(Progress(1900, 80, 320.82f, "aleck@gmail.com"))
-            data.add(Progress(1250, 65, 263.22f, "aleck@gmail.com"))
-            data.add(Progress(500, 20, 70.98f, "aleck@gmail.com"))
+            data.add(Progress("Walking",1000, 60, 250.78f, "aleck@gmail.com"))
+            data.add(Progress("Jogging",750, 60, 120.56f, "aleck@gmail.com"))
+            data.add(Progress("Walking",1300, 70, 270.32f, "aleck@gmail.com"))
+            data.add(Progress("Walking",1900, 80, 320.82f, "aleck@gmail.com"))
+            data.add(Progress("Jogging",1250, 65, 263.22f, "aleck@gmail.com"))
+            data.add(Progress("Jogging",500, 20, 70.98f, "aleck@gmail.com"))
 
             return data
         }
