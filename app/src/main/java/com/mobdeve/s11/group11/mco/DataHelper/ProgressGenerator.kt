56550207d07f7d030986e582b766ba22086d188d
Klaexis <alecklim1245@gmail.com>
2023-03-29 00:55:17 +0800

    Update ProgressGenerator.kt

--- app/src/main/java/com/mobdeve/s11/group11/mco/DataHelper/ProgressGenerator.kt
+++ app/src/main/java/com/mobdeve/s11/group11/mco/DataHelper/ProgressGenerator.kt
@@ -7,12 +7,12 @@ class ProgressGenerator {
         fun loadProgress(): ArrayList<Progress> {
             val data = ArrayList<Progress>()
 
-            data.add(Progress(0,"Walking",1000, 60, 250.78f, "aleck@gmail.com", "March 20, 2023"))
-            data.add(Progress(1,"Jogging",750, 60, 120.56f, "aleck@gmail.com", "March 21, 2023"))
-            data.add(Progress(2,"Walking",1300, 70, 270.32f, "aleck@gmail.com", "March 22, 2023"))
-            data.add(Progress(3,"Walking",1900, 80, 320.82f, "aleck@gmail.com", "March 23, 2023"))
-            data.add(Progress(4,"Jogging",1250, 65, 263.22f, "aleck@gmail.com", "March 24, 2023"))
-            data.add(Progress(5,"Jogging",500, 20, 70.98f, "aleck@gmail.com", "March 25, 2023"))
+            data.add(Progress("Walking",1000, 60, 250.78f, "March 20, 2023", "aleck@gmail.com", 0))
+            data.add(Progress("Jogging",750, 60, 120.56f,  "March 21, 2023", "aleck@gmail.com", 1))
+            data.add(Progress("Walking",1300, 70, 270.32f,  "March 22, 2023", "aleck@gmail.com", 2))
+            data.add(Progress("Walking",1900, 80, 320.82f, "March 23, 2023", "aleck@gmail.com",  3))
+            data.add(Progress("Jogging",1250, 65, 263.22f,  "March 24, 2023", "aleck@gmail.com", 4))
+            data.add(Progress("Jogging",500, 20, 70.98f,  "March 25, 2023", "aleck@gmail.com", 5))
 
             return data
         }
